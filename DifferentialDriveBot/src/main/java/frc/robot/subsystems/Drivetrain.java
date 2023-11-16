f2ff7a85b567ffa2b5bc52b46a144dcd4f08be97
kevin jaramillo <kevinjaramillo247@gamil.com>
2023-11-16 10:13:49 -0800

    update drivetrain subsystem

--- DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,26 +4,39 @@
 
 package frc.robot.subsystems;
 
-
+//Drivetrain imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.math.filter.SlewRateLimiter; 
+
 
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
 
   //Motor variables
-  private final leftLeadMotor;
-  private final rightLeadMotor;
+  private final CANSparkMax leftLeadMotor;
+  private final CANSparkMax rightLeadMotor;
 
-  private final leftFollower;
-  private final rightFollower;
+  private final CANSparkMax leftFollower;
+  private final CANSparkMax rightFollower;
 
+  //Motor group variables
+  private MotorControllerGroup leftMotors;
+  private MotorControllerGroup rightMotors;
+  
+  // Create filters for slew rate limiting
+  SlewRateLimiter turnFilter;
+  SlewRateLimiter driveFilter;
   
 
+  public Drivetrain() {
 
-  
 
-  public Drivetrain() {}
+  }
 
   @Override
   public void periodic() {
