05c31618d34bb5b3a8b65789863417bcd646fe9b
Kevin <kevinjaramillo247@gmail.com>
2023-11-17 15:38:53 -0800

    Continue Drivetrain and Constants file

--- DifferentialDriveBot/src/main/java/frc/robot/Constants.java
+++ DifferentialDriveBot/src/main/java/frc/robot/Constants.java
@@ -1,10 +1,32 @@
 package frc.robot;
 
+import java.util.ArrayList;
+
 import com.revrobotics.CANSparkMax;
 
 
 
 
+
 public final class Constants {
+
+    public static ArrayList<CANSparkMax> SPARK_LIST = new ArrayList<CANSparkMax>();
+
+    public final static class ControllerConstants{
+
+    }
     
+    public final static class DrivetrainConstants{
+
+
+        //Motor CAN ID Constants
+        public static final int LEFT_LEAD_MOTOR_CAN_ID = 2;
+        public static final int LEFT_FOLLOWER_MOTOR_CAN_ID = 3;
+
+        public static final int RIGHT_LEAD_MOTOR_CAN_ID = 1;
+        public static final int RIGHT_FOLLOWER_MOTOR_CAN_ID = 0;
+
+        public static final boolean RIGHT_MOTORS_INVERT = false;
+        public static final boolean LEFT_MOTORS_INVERT = false;
+    }
 }
--- DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
 import frc.robot.Constants;
+import frc.robot.Constants.DrivetrainConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -25,9 +26,9 @@ public class Drivetrain extends SubsystemBase {
 
   //Creates Motor variables
   private final CANSparkMax leftLeadMotor;
-  private final CANSparkMax rightLeadMotor;
-
   private final CANSparkMax leftFollower;
+
+  private final CANSparkMax rightLeadMotor;
   private final CANSparkMax rightFollower;
 
   //Creates Motor group variables
@@ -50,12 +51,61 @@ public class Drivetrain extends SubsystemBase {
     driveFilter = new SlewRateLimiter();
     turnFilter = new SlewRateLimiter();
 
+    //Initialize Motors
+    leftLeadMotor = new CANSparkMax(DrivetrainConstants.LEFT_LEAD_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushlesss);
+    leftFollower = new CANSparkMax(DrivetrainConstants.LEFT_FOLLOWER_MOTOR_CAN_ID,CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    rightLeadMotor = new CANSparkMax(DrivetrainConstants.RIGHT_LEAD_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightFollower = new CANSparkMax(DrivetrainConstants.RIGHT_FOLLOWER_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    leftLeadMotor.restoreFactoryDefaults();
+    leftFollower.restoreFactoryDefaults();
+    rightLeadMotor.restoreFactoryDefaults();
+    rightFollower.restoreFactoryDefaults();
+
+    //Setting Up Left And Right Encoders
+    leftEncoder = leftLeadMotor.getEncoder();
+    leftEncoder.setPositionConversionFactor();
+    leftEncoder.setVelocityConversionFactor();
+
+    rightEncoder = rightLeadMotor.getEncoder();
+    rightEndocer.setPositionConversionFactor();
+    rightEncoder.setVelocityConversionFactor();
+
+    leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
+
+    leftLeadMotor.setInverted(true);
+    rightLeadMotor.setInverted(false);
+
+    //Sets All Motors To Break Mode
+    leftLeadMotor.setIdleModer(CANSparkMax.IdleMode.kBrake);
+    leftFollower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    rightLeadMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    rightFollower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    //Follow Lead Motors
+    leftFollower.follow(leftLeadMotor, DrivetrainConstants.LEFT_MOTOR_INVERT);
+    rightFollower.follow(rightLeadMotor, DrivetrainConstants.RIGHT_MOTORS_INVERT);
 
 
+    Constants.SPARK_LIST.add(leftLeadMotor);
+    Constants.SPARK_LIST.add(leftFollower);
+    Constants.SPARK_LIST.add(rightLeadMotor);
+    Constants.SPARK_LIST.add(rightFollower);
+
+    //Initialize Motor Groups
+    leftMotors = new MotorControllerGroup(leftFollower, leftLeadMotor);
+    rightMotors = new MotorControllerGroup(rightFollower, rightLeadMotor);
+
+    //Sets Drive Subsystem
+    drive = new DifferentialDrive(leftMotors, rightMotors);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    
   }
 }
