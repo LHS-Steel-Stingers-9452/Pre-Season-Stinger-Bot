189e4a2ef7d97325e4a2cb9a2e8d00a82ea7fb0c
Kevin <kevinjaramillo247@gmail.com>
2023-11-26 18:03:30 -0800

    Basic teleop Drivetrain subsytem complete Finish Auton methods and calculations

--- DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ DifferentialDriveBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -16,20 +16,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.math.filter.SlewRateLimiter; 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+//math loolololol
+import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.ADIS16470_IMU; // replace with pigeon 2
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
-
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
@@ -37,8 +32,8 @@ public class Drivetrain extends SubsystemBase {
   //Creates Differential Drive
   private final DifferentialDrive drive;
 
-  //Creates Differential Drive Kinematics
-  private final DifferentialDriveKinematics kinematics;
+  //private Field2d field;
+  private static Drivetrain instance;
 
   //Creates Motor variables
   private final CANSparkMax leftLeadMotor;
@@ -53,44 +48,40 @@ public class Drivetrain extends SubsystemBase {
 
   //Creates Motor Encoders
   private RelativeEncoder leftEncoder;
-  private RelativeEncoder rightEndocer;
+  private RelativeEncoder rightEncoder;
+
+  //creates Pigeon gyro
+  private final Pigeon2 gyro;
+
+  //Creates Forward and Turn Vars For Speed Input
+  private double forward;
+  private double turn; 
 
   // Create Filters For Slew Rate Limiting
   SlewRateLimiter turnFilter;
   SlewRateLimiter driveFilter;
 
-   //Creates Odometry
-   private final Odometry odometry;
+  //private final DifferentialDriveKinematics kinematics;
+  //private final DifferentialDriveOdometry odometry;
 
-  //Create Ramsete Controller
-  private final RamseteController ramseteController;
-
-  private  double forward;
-  private  double turn;
-
-  private SimpleMotorFeedforward feedforward;
-
-  private PIDController lefPidController;
-  private PIDController righPidController;
-
-  private final ADIS16470_IMU gyro;
-
-  private Field2d field;
-
-  private static Drivetrain instance;
 
   public Drivetrain() {
 
-    gyro = new ADIS16470_IMU(); //replace with pigeon 2
-    field = new Field2d();
+    gyro = new Pigeon2(DrivetrainConstants.PIGEON_GYRO_CAN_ID);
+    //field = new Field2d();
 
     //Initialize Motors
-    leftLeadMotor = new CANSparkMax(DrivetrainConstants.LEFT_LEAD_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushlesss);
-    leftFollower = new CANSparkMax(DrivetrainConstants.LEFT_FOLLOWER_MOTOR_CAN_ID,CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftLeadMotor = new CANSparkMax(DrivetrainConstants.LEFT_LEAD_MOTOR_CAN_ID,
+          CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftFollower = new CANSparkMax(DrivetrainConstants.LEFT_FOLLOWER_MOTOR_CAN_ID,
+          CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    rightLeadMotor = new CANSparkMax(DrivetrainConstants.RIGHT_LEAD_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-    rightFollower = new CANSparkMax(DrivetrainConstants.RIGHT_FOLLOWER_MOTOR_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightLeadMotor = new CANSparkMax(DrivetrainConstants.RIGHT_LEAD_MOTOR_CAN_ID, 
+          CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightFollower = new CANSparkMax(DrivetrainConstants.RIGHT_FOLLOWER_MOTOR_CAN_ID, 
+          CANSparkMaxLowLevel.MotorType.kBrushless);
 
+    //Restore As A Saftey Measure
     leftLeadMotor.restoreFactoryDefaults();
     leftFollower.restoreFactoryDefaults();
     rightLeadMotor.restoreFactoryDefaults();
@@ -98,13 +89,14 @@ public class Drivetrain extends SubsystemBase {
 
     //Setting Up Left And Right Encoders
     leftEncoder = leftLeadMotor.getEncoder();
-    leftEncoder.setPositionConversionFactor();
-    leftEncoder.setVelocityConversionFactor();
+    leftEncoder.setPositionConversionFactor(DrivetrainConstants.DRIVING_ENCODER_POS_FACTOR);
+    leftEncoder.setVelocityConversionFactor(DrivetrainConstants.DRIVING_ENCODER_VEL_FACTOR);
 
     rightEncoder = rightLeadMotor.getEncoder();
-    rightEndocer.setPositionConversionFactor();
-    rightEncoder.setVelocityConversionFactor();
+    rightEncoder.setPositionConversionFactor(DrivetrainConstants.DRIVING_ENCODER_POS_FACTOR);
+    rightEncoder.setVelocityConversionFactor(DrivetrainConstants.DRIVING_ENCODER_VEL_FACTOR);
 
+    //Reset As A Saftery Measure
     leftEncoder.setPosition(0);
     rightEncoder.setPosition(0);
 
@@ -112,14 +104,14 @@ public class Drivetrain extends SubsystemBase {
     rightLeadMotor.setInverted(false);
 
     //Set All Motors To Break Mode
-    leftLeadMotor.setIdleModer(CANSparkMax.IdleMode.kBrake);
+    leftLeadMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
     leftFollower.setIdleMode(CANSparkMax.IdleMode.kBrake);
     rightLeadMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
     rightFollower.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
-    //Follow Lead Motors
-    leftFollower.follow(leftLeadMotor, DrivetrainConstants.LEFT_MOTORS_INVERT);
-    rightFollower.follow(rightLeadMotor, DrivetrainConstants.RIGHT_MOTORS_INVERT);
+     //Follow Lead Motors
+     leftFollower.follow(leftLeadMotor, DrivetrainConstants.LEFT_MOTORS_INVERT);
+     rightFollower.follow(rightLeadMotor, DrivetrainConstants.RIGHT_MOTORS_INVERT);
 
     //Add motors after initilization to SPARK_LIST *Saves Parameters
     Constants.SPARK_LIST.add(leftLeadMotor);
@@ -134,25 +126,53 @@ public class Drivetrain extends SubsystemBase {
     //Sets Drive Subsystem
     drive = new DifferentialDrive(leftMotors, rightMotors);
 
+    /*
+    Math stuff for Auton 
     //Create A Field For Displaying Robot Position Nn The Dashboard
     SmartDashboard.putData("Field", this.field);
 
     //Math Related Code Goes Here
     //kinumatics
-    //Feedforward
+    kinematics = new DifferentialDriveKinematics(DrivetrainConstants.TRACKWIDTH);
+    //odometry
+    //odometry = new DifferentialDriveOdometry()
+    //feedforward
     //PID Controllers
     //Slew Rate Limiters
     driveFilter = new SlewRateLimiter();
     turnFilter = new SlewRateLimiter();
     //Ramsete Controllers
+    */
+
+    //Slew Rate Limiters
+    driveFilter = new SlewRateLimiter(DrivetrainConstants.SLEW_RATE_DRIVE_POSITIVE,
+      DrivetrainConstants.SLEW_RATE_DRIVE_NEGATIVE, 0);
+    turnFilter = new SlewRateLimiter(DrivetrainConstants.SLEW_RATE_TURN_POSITIVE,
+      DrivetrainConstants.SLEW_RATE_TURN_NEGATIVE, 0);
+
   }
+
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-
     double slewForward = driveFilter.calculate(forward);
 
-    drive.arcadeDrive();
+    drive.arcadeDrive(
+      DriverStation.isAutonomous() ? forward: slewForward, 
+      turn);
+  }
+
+  public static synchronized Drivetrain getInstance() {
+    if (instance == null) {
+        instance = new Drivetrain();
+    }
+    return instance;
+  }
+  public void drive(double forward, double turn) {
+    this.forward = -forward;
+    this.turn = turn;
+    drive.feed();
   }
 }
