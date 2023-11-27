92ad2e7976b9f579487cc6493c5292ecd75d8069
Kevin <kevinjaramillo247@gmail.com>
2023-11-26 18:02:15 -0800

    Started binding commands and subsystems

--- DifferentialDriveBot/src/main/java/frc/robot/RobotContainer.java
+++ DifferentialDriveBot/src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,42 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.commands.BaseDrive;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
-  private final Drivetrain drivetrain = new Drivetrain();
+  //Subsystems
+  private final Drivetrain drivetrain;
+  private final XboxController driverController;
+  
+
+  //Commands
+  private final BaseDrive baseDrive;
 
   public RobotContainer() {
     configureBindings();
 
-    //drivetrain.setDefaultCommand(); --> for later use
+    drivetrain = new Drivetrain();
+    driverController = new XboxController(0);
+
+    baseDrive = new BaseDrive(drivetrain,
+      () -> MathUtil.applyDeadband(driverController.getLeftY(),
+        ControllerConstants.DRIVER_FORWARD_DEADBAND),
+      () -> MathUtil.applyDeadband(driverController.getRightX() * 0.8, 
+        ControllerConstants.DRIVER_TURN_DEADBAND));
+    
+
+    drivetrain.setDefaultCommand(baseDrive);
+
   }
 
   private void configureBindings() {
