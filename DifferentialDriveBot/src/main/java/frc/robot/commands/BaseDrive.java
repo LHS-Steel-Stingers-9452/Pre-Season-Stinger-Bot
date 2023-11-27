0be03d7f6a23610d53af5d3fc0f31c76f8affd76
Kevin <kevinjaramillo247@gmail.com>
2023-11-26 18:02:46 -0800

    Started experimenting with BaseDrive command

---
+++ DifferentialDriveBot/src/main/java/frc/robot/commands/BaseDrive.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class BaseDrive extends CommandBase{
+    private final Drivetrain drive;
+    private final DoubleSupplier forward;
+    private final DoubleSupplier turn;
+
+    public BaseDrive(Drivetrain drive, DoubleSupplier forward, DoubleSupplier turn){
+        this.drive = drive;
+        this.forward = forward;
+        this.turn = turn;
+        addRequirements(drive);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        drive.drive(forward.getAsDouble()*1, turn.getAsDouble()*1);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        Commands.runOnce(() -> Drivetrain.getInstance().drive( 0, 0));
+    }
+
+    // Returns true when the command should end.
+    // We never want this command to end, so it always returns false
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
