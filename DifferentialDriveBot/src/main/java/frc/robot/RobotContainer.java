52ffe9bac94c19bb85933c621894cf54fd4a9c52
kevin <kevinjaramillo247@gamil.com>
2024-01-06 11:23:36 -0800

    Switching branches

--- DifferentialDriveBot/src/main/java/frc/robot/RobotContainer.java
+++ DifferentialDriveBot/src/main/java/frc/robot/RobotContainer.java
@@ -20,10 +20,24 @@ import edu.wpi.first.wpilibj.XboxController;
 //import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 //import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+//vision
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
 public class RobotContainer {
   //Subsystems
   private final Drivetrain drivetrain;
   private final XboxController driverController;
+
+  
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
   
 
   //Commands
@@ -44,6 +58,18 @@ public class RobotContainer {
 
     RobotContainer.incinerateMotors();
 
+    //read values periodically
+    
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+    
+    
   }
 
   private void configureBindings() {
